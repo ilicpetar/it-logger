332c7bc85fc1c43af9bc101c93d25c09cbd205b7
ilic.petar <ilic.petar@gmail.com>
2021-03-09 03:30:39 +0100

    first commit

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div className="progress blue lighten-4">
+      <div className="indeteminate blue"></div>
+    </div>
+  );
+};
+
+export default Preloader;
---
+++ src/components/logs/Logs.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import LogItem from "./LogsItem";
+import Preloader from "./../layout/Preloader";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getLogs } from "./../../actions/logActions";
+
+const Logs = ({ log: { logs, loading }, getLogs }) => {
+  useEffect(() => {
+    getLogs();
+  }, []);
+
+  // const getLogs = async () => {
+  //   setLoading(true);
+  //   const res = await fetch("/logs");
+  //   const data = await res.json();
+
+  //   setLogs(data);
+  //   setLoading(false);
+  // };
+
+  if (loading || logs === null) {
+    return <Preloader />;
+  }
+
+  return (
+    <ul className="collection with-header">
+      <li className="collection-header">
+        <h4 className="center">System Logs</h4>
+      </li>
+      {!loading && logs.length === 0 ? (
+        <p className="center">No logs to show ... </p>
+      ) : (
+        logs.map((log) => <LogItem key={log.id} log={log} />)
+      )}
+    </ul>
+  );
+};
+
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
